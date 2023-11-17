880a22239cdc1561a70a2e2b9a0e3547b2377135
Joel Santana <joelsantzdev@gmail.com>
2023-11-17 02:13:22 -0500

    feat(components): add footer

---
+++ app/components/shared/Footer.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FooterContainer = styled.footer`
+  text-align: center;
+  padding: 20px;
+  margin-top: 30px;
+  border-top: 1px solid #ddd;
+`;
+
+const FooterText = styled.p`
+  margin: 0;
+  color: #666;
+`;
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterText>Made by Joel Santana for Million and Up Technical Test</FooterText>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@
 import { Provider } from "react-redux"
 import { CryptoList } from "./components/CrytoList"
 import { store } from "./store/store"
+import Footer from "./components/shared/Footer"
 
 export default function App() {
   return (
       <Provider store={store}>
           <CryptoList />
+          <Footer />
       </Provider>
   )
 }
